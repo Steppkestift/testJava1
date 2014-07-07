392f02e271c83c61282ae9c4f1ff1840a5a2774f
steppkestift <heiko.linke@yahoo.de>
2014-07-07 15:00:41 +0200

    Initial commit

---
+++ src/testJava.java
@@ -0,0 +1,10 @@
+/**
+ * Created by heiko-linke on 07.07.14.
+ */
+public class testJava {
+    public static void main(String[ ] args)
+    {
+        // Ausgabe Hello World!
+        System.out.println("Hello World!");
+    }
+}
