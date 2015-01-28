2c0f460793f4bbd2b16185cf54fd8218f4db17ae
Steppkestift <heiko.linke@posteo.de>
2015-01-28 19:16:30 +0100

    test2

--- src/CoffeeTest1.java
+++ src/CoffeeTest1.java
@@ -1,16 +1,29 @@
-/**
- * Created by heiko-linke on 28.01.15.
- */
-enum CoffeeSize { BIG, HUGE, OVERWHELMING}
+enum CoffeeSize {
+    BIG(8), HUGE(10), OVERWHELMING(16);
 
-class Coffee {
-        CoffeeSize size;
-}
 
-public class CoffeeTest1 {
-    public static void main(String[] args)
-    {
-       Coffee drink = new Coffee();
-       drink.size = CoffeeSize.BIG;
+    CoffeeSize(int ounces) {
+        this.ounces = ounces;
     }
+
+
+private int ounces;
+public int getOunces() {
+    return ounces;
+}
+}
+public class CoffeeTest1 {
+    CoffeeSize size;
+    public static void main(String[] args) {
+        CoffeeTest1 drink1 = new CoffeeTest1();
+        drink1.size = CoffeeSize.BIG;
+        CoffeeTest1 drink2 = new CoffeeTest1();
+        drink2.size = CoffeeSize.OVERWHELMING;
+
+    System.out.println(drink1.size.getOunces());
+        for (CoffeeSize cs: CoffeeSize.values())
+            System.out.print(cs + " " + cs.getOunces());
+
+    }
+
 }
